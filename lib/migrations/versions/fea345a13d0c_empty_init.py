458f23b4cd490ba281edfce226809e07747c4272
Ricardo Guerra <RikkuG491@gmail.com>
2023-06-04 23:15:01 -0400

    Added solution code

---
+++ lib/migrations/versions/fea345a13d0c_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: fea345a13d0c
+Revises: 
+Create Date: 2023-06-04 22:59:00.269949
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'fea345a13d0c'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
